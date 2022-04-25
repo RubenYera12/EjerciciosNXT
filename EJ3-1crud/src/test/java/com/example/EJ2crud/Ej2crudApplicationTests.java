16e617eb4550a20bf026c99bda43ea6f299fb4e2
Rubén María Yera Martín <rubenmaria.yera@bosonit.com>
2022-04-25 16:31:28 +0200

    CRUD con relaciones en diferentes tablas

---
+++ EJ3-1crud/src/test/java/com/example/EJ2crud/Ej2crudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EJ2crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ej2crudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
