21bc92002018bb9f160b090a4413c78228898fa3
Rubén María Yera Martín <rubenmaria.yera@bosonit.com>
2022-04-26 11:39:43 +0200

    BS9 RestTemplate & Feign

---
+++ BS9/src/main/java/com/example/EJ2crud/Ej2crudApplication.java
@@ -0,0 +1,14 @@
+package com.example.EJ2crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class Ej2crudApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Ej2crudApplication.class, args);
+  }
+}
